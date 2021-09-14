03bd631f8b180da7401be30cfa0c0de169b89bec
Isaac Cruz <isaac.clm1@gmail.com>
2021-09-14 20:43:26 -0300

    Updates

---
+++ precache-manifest.4d8cd7db2df51c855af8ebf7b2b6391d.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59039a22ede1cc38874b075c5d5d7f33",
+    "url": "/ReceitApp/index.html"
+  },
+  {
+    "revision": "936f2a59b4efd5f121c4",
+    "url": "/ReceitApp/static/css/2.fa6c921b.chunk.css"
+  },
+  {
+    "revision": "8fdb347f0183282d0400",
+    "url": "/ReceitApp/static/css/main.2fb603df.chunk.css"
+  },
+  {
+    "revision": "936f2a59b4efd5f121c4",
+    "url": "/ReceitApp/static/js/2.bfa81aa5.chunk.js"
+  },
+  {
+    "revision": "57fb86a905f06aed4b5091745659b335",
+    "url": "/ReceitApp/static/js/2.bfa81aa5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fdb347f0183282d0400",
+    "url": "/ReceitApp/static/js/main.ed515f90.chunk.js"
+  },
+  {
+    "revision": "2b62814af906ba4acd0b",
+    "url": "/ReceitApp/static/js/runtime-main.297a30fd.js"
+  },
+  {
+    "revision": "fd159fa36cc221651b655a085f12070c",
+    "url": "/ReceitApp/static/media/background.fd159fa3.jpg"
+  },
+  {
+    "revision": "b89133460c9aef111d6e773396b5917d",
+    "url": "/ReceitApp/static/media/blackHeartIcon.b8913346.svg"
+  },
+  {
+    "revision": "f87c47c37c0c0530fe7307af260caa25",
+    "url": "/ReceitApp/static/media/drinkIcon.f87c47c3.svg"
+  },
+  {
+    "revision": "157945fffa619b919f7bb65fe11b7b43",
+    "url": "/ReceitApp/static/media/exploreIcon.157945ff.svg"
+  },
+  {
+    "revision": "ecee03f3524e9d6a23ed5f3e4f186b32",
+    "url": "/ReceitApp/static/media/logoreceitapp.ecee03f3.png"
+  },
+  {
+    "revision": "b27ba19ea033355b73926443af1756e9",
+    "url": "/ReceitApp/static/media/mealIcon.b27ba19e.svg"
+  },
+  {
+    "revision": "74e487e504d5cf16d7832bef2fbacfea",
+    "url": "/ReceitApp/static/media/profileIcon.74e487e5.svg"
+  },
+  {
+    "revision": "db81213596cb3beb56440bb39aa70c8c",
+    "url": "/ReceitApp/static/media/searchIcon.db812135.svg"
+  },
+  {
+    "revision": "06215ed4cae23455c02abc804445ba74",
+    "url": "/ReceitApp/static/media/shareIcon.06215ed4.svg"
+  },
+  {
+    "revision": "ea3b6ba893ba3908dd84f6170b75545e",
+    "url": "/ReceitApp/static/media/whiteHeartIcon.ea3b6ba8.svg"
+  }
+]);
